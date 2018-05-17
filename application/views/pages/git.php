6649fbd08750f31590f3f3434ea739e4d41d997a
j.alejandro.1290 <j.alejandro.1290@gmail.com>
2018-05-17 12:54:56 -0400

    Git Commands Page Created

---
+++ application/views/pages/git.php
@@ -0,0 +1,41 @@
+<html>
+  <head>
+    <title>Git Commands</title>
+    <style>
+      table tr td, table tr th{
+        border:solid black 1px;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>
+      Git Commands
+    </h1>
+    <table>
+      <tr>
+        <th>Commit</th>
+        <td>$git commit -m '/&lt;comment/&gt;'</td>
+      </tr>
+      <tr>
+        <th>Add all</th>
+        <td>$git add .</td>
+      </tr>
+      <tr>
+        <th>See Commits</th>
+        <td>$git log</td>
+      </tr>
+      <tr>
+        <th>Roll Back in New Branch</th>
+        <td>$git checkout -b /&lt;new-branch-name/&gt; /&lt;commit-id/&gt;</td>
+      </tr>
+      <tr>
+        <th>Check the branchs</th>
+        <td>$git branch</td>
+      </tr>
+      <tr>
+        <th>Check status for commits</th>
+        <td>$git status</td>
+      </tr>
+    </table>
+  </body>
+</html>
